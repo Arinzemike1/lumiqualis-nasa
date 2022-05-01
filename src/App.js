c11dddbbaa8624951ac7e91c8b5717c77df6f33a
Arinze <drealzorix@gmail.com>
2022-05-01 14:52:48 +0100

    Add app routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MovieListing from './components/MovieListing';
+import MovieDetail from './components/MovieDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<MovieListing />} />
+          <Route path="/movie/:movieId" exact element={<MovieDetail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
